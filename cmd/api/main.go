e8aa5629476e043142f0804fa5196a98de0f055f
van1dal <van1dalgrr@gmail.com>
2026-09-19 15:13:29 +0300

    feat: add application config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+
+	"orderApi/internal/config"
 )
 
 func main() {
-	fmt.Println("Server is started")
+	cfg, err := config.Load("internal/config/config.yml")
+
+	if err != nil {
+		panic(fmt.Errorf("failed to load config: %w", err))
+	}
+	slog.Info("configuration has been loaded", "port", cfg.Server.Port)
 }
